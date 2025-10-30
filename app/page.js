9b4ec8ed173df0c07f1b902339ccd8bd0e3c735b
Andrej Koller <andrejkoller@outlook.com>
2025-10-30 10:20:55 +0100

    Update styles and layout for particle component; remove unused SVG files

--- app/page.js
+++ app/page.js
@@ -1,65 +1,42 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="h-screen w-screen bg-white flex justify-center">
+      <div className="w-115 h-110  p-5">
+        <h1 className="text-4xl text-black font-playfair italic">Hey,</h1>
+
+        <div className="w-full h-50 mt-3 rounded-md grid grid-cols-25 grid-rows-10">
+          {Array.from({ length: 250 }).map((_, index) => (
+            <div key={index} className="flex -inset-4">
+              <p className="text-2xl font-bold text-neutral-400 transition-all duration-0 hover:text-black cursor-default hover:delay-0 delay-500 ease-in-out hover:scale-120 ">
+                +
+              </p>
+            </div>
+          ))}
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <p className="mt-5 mb-2 tracking-tight text-[23px] font-ios text-neutral-800">
+          We help <span className="italic"> founders </span> launch their site
+          in 2 weeks from
+          <span className="text-blue-500 "> copy</span>,
+          <span className="text-red-500 "> brand</span>,
+          <span className="text-green-500 "> tagline</span>,{" "}
+          <span className="text-yellow-400 "> research</span>,
+          <span className="text-purple-500 "> visual</span> all done by us.
+        </p>
+        <p className="tracking-tighter text-neutral-400 font-playfair italic text-md">
+          speed and building premium site is our superpower
+        </p>
+
+        <div className="flex gap-2">
+          <button className="text-black font-playfair rounded-xl mt-3  border-2 border-neutral-200 py-1 px-3.5 italic text-[15px] font-semibold tracking-tight hover:border-neutral-400 transition-all duration-200 ">
+            Book a call
+          </button>
+
+          <button className="text-white bg-black font-playfair rounded-xl mt-3 px-5 italic text-[15px] hover:bg-neutral-700 transition-all duration-200 tracking-tight">
+            $9k/mo →
+          </button>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
